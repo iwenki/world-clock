f2a96821d8b78a68edae6414f61f1229f84834cd
iwenki <139893979+iwenki@users.noreply.github.com>
2023-09-24 16:52:06 -0500

    Added country flags

--- src/script.js
+++ src/script.js
@@ -4,7 +4,9 @@ function updateTimes() {
     let spainTimeElement = spainCard.querySelector(".time");
     let spainDateElement = spainCard.querySelector(".date");
     let spainTime = moment().tz("Europe/Madrid");
-    spainTimeElement.innerHTML = spainTime.format("hh:mm:ss:SSS a");
+    spainTimeElement.innerHTML = spainTime.format(
+      "hh:mm:ss:SSS [<small>]A[</small>]"
+    );
     spainDateElement.innerHTML = spainTime.format("dddd, MMMM Do, YYYY");
   }
   let usaCard = document.querySelector("#usa");
@@ -12,7 +14,9 @@ function updateTimes() {
     let usaTimeElement = usaCard.querySelector(".time");
     let usaDateElement = usaCard.querySelector(".date");
     let usaTime = moment().tz("America/Chicago");
-    usaTimeElement.innerHTML = usaTime.format("hh:mm:ss:SSS a");
+    usaTimeElement.innerHTML = usaTime.format(
+      "hh:mm:ss:SSS [<small>]A[</small>]"
+    );
     usaDateElement.innerHTML = usaTime.format("dddd, MMMM Do, YYYY");
   }
   let guatemalaCard = document.querySelector("#guatemala");
@@ -20,7 +24,9 @@ function updateTimes() {
     let guatemalaTimeElement = guatemalaCard.querySelector(".time");
     let guatemalaDateElement = guatemalaCard.querySelector(".date");
     let guatemalaTime = moment().tz("America/Guatemala");
-    guatemalaTimeElement.innerHTML = guatemalaTime.format("hh:mm:ss:SSS a");
+    guatemalaTimeElement.innerHTML = guatemalaTime.format(
+      "hh:mm:ss:SSS [<small>]A[</small>]"
+    );
     guatemalaDateElement.innerHTML = guatemalaTime.format(
       "dddd, MMMM Do, YYYY"
     );
@@ -30,7 +36,9 @@ function updateTimes() {
     let southKoreaTimeElement = southKoreaCard.querySelector(".time");
     let southKoreaDateElement = southKoreaCard.querySelector(".date");
     let southKoreaTime = moment().tz("Asia/Seoul");
-    southKoreaTimeElement.innerHTML = southKoreaTime.format("hh:mm:ss:SSS a");
+    southKoreaTimeElement.innerHTML = southKoreaTime.format(
+      "hh:mm:ss:SSS [<small>]A[</small>]"
+    );
     southKoreaDateElement.innerHTML = southKoreaTime.format(
       "dddd, MMMM Do, YYYY"
     );
@@ -40,21 +48,21 @@ function updateTimes() {
     let kenyaTimeElement = kenyaCard.querySelector(".time");
     let kenyaDateElement = kenyaCard.querySelector(".date");
     let kenyaTime = moment().tz("Africa/Nairobi");
-    kenyaTimeElement.innerHTML = kenyaTime.format("hh:mm:ss:SSS a");
+    kenyaTimeElement.innerHTML = kenyaTime.format(
+      "hh:mm:ss:SSS [<small>]A[</small>]"
+    );
     kenyaDateElement.innerHTML = kenyaTime.format("dddd, MMMM Do, YYYY");
   }
-  activateApi();
+ 
 }
 function updateCity(event) {
-  function individualCities() {
     let timeZone = event.target.value;
-    if (timeZone === "local") {
-      timeZone = moment.tz.guess();
-    }
+    if(timeZone === "local") {
+      timeZone = moment.tz.guess();}
     let cityName = timeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(timeZone);
     let cities = document.querySelector("#indexCities");
-    if (timeZone.length > 0) {
+    if (timeZone === "America/Denver" || timeZone=== "America/Los_Angeles") {
       cities.innerHTML = `<div
           class="card text-bg mb-3 index-card"
           style="max-width: 36rem"
@@ -63,41 +71,242 @@ function updateCity(event) {
           <div class="card-body">
             <div class="row">
               <div class="col">
-                <span class="time"> ${cityTime.format("hh:mm:ss:SSS a")}</span>
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
                 <p class="card-text date">${cityTime.format(
                   "dddd, MMMM Do, YYYY"
                 )}</p>
               </div>
-              <div class="col">
-                <span class="weather-icon"></span>
-                <div class="temperature"></div>
-                <p class="card-text description">
-                </p>
+               <div class="col">
+                <img class="flags"src="images/usa.png"/>
               </div>
             </div>
           </div>
-        </div><a href="/">Return to main</a>`;
+        </div>
+              `;
+    } else if(
+      timeZone==="America/Sao_Paulo"
+    ){
+      cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+        >
+          <div class="card-header city-name">${cityName} </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
+                <p class="card-text date">${cityTime.format(
+                  "dddd, MMMM Do, YYYY"
+                )}</p>
+              </div>
+               <div class="col">
+                <img class="flags"src="images/brazil.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+              `;
+    }
+    else if(
+      timeZone==="Asia/Kathmandu"
+    ){
+      cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+        >
+          <div class="card-header city-name">${cityName} </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
+                <p class="card-text date">${cityTime.format(
+                  "dddd, MMMM Do, YYYY"
+                )}</p>
+              </div>
+               <div class="col">
+                <img class="flags"src="images/nepal.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+              `;
+    }
+    else if(
+      timeZone==="Australia/Perth"
+    ){
+     cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+        >
+          <div class="card-header city-name">${cityName} </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
+                <p class="card-text date">${cityTime.format(
+                  "dddd, MMMM Do, YYYY"
+                )}</p>
+              </div>
+               <div class="col">
+                <img class="flags"src="images/australia.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+              `; 
+    }
+    else if(
+      timeZone==="Asia/Dubai"
+    ){
+      cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+        >
+          <div class="card-header city-name">${cityName} </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
+                <p class="card-text date">${cityTime.format(
+                  "dddd, MMMM Do, YYYY"
+                )}</p>
+              </div>
+               <div class="col">
+                <img class="flags"src="images/uae.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+              `; 
+    }
+    else if(
+      timeZone.length>0) {
+      cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+        >
+          <div class="card-header city-name">${cityName} </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"> ${cityTime.format(
+                  "hh:mm [<small>]A[</small>]"
+                )}</span>
+                <p class="card-text date">${cityTime.format(
+                  "dddd, MMMM Do, YYYY"
+                )}</p>
+              </div>
+               <div class="col">
+              </div>
+            </div>
+          </div>
+        </div>
+              `; 
+    }
+    else {
+      cities.innerHTML = `<div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+          id="spain"
+        >
+          <div class="card-header city-name">Barcelona, Spain</div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"></span>
+                <p class="card-text date"></p>
+              </div>
+              <div class="col">
+                <img class="flags"src="images/spain.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+          id="usa"
+        >
+          <div class="card-header city-name">Houston, United States of America 
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"></span>
+                <p class="card-text date"></p>
+              </div>
+              <div class="col">
+                <img class="flags"src="images/usa.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+          id="guatemala"
+        >
+          <div class="card-header city-name">Guatemala City, Guatemala</div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"></span>
+                <p class="card-text date"></p>
+              </div>
+              <div class="col">
+                <img class="flags"src="images/guatemala.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+          id="southKorea"
+        >
+          <div class="card-header city-name">Seoul, South Korea</div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"></span>
+                <p class="card-text date"></p>
+              </div>
+              <div class="col">
+                <img class="flags"src="images/koreasouth.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card text-bg mb-3 index-card"
+          style="max-width: 36rem"
+          id="kenya"
+        >
+          <div class="card-header city-name">Nairobi, Kenya</div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <span class="time"></span>
+                <p class="card-text date"></p>
+              </div>
+              <div class="col">
+                <img class="flags"src="images/kenya.png"/>
+              </div>
+            </div>
+          </div>
+        </div>`;
     }
   }
-  setInterval(individualCities, 1);
-  individualCities();
-}
-
-function activateApi() {
-  let cityName = document.querySelector(".justCityName");
-  const apiKey = "cbc90ba0a21t28a990f44b7f6f3ea68o";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-}
-function handleResponse(response) {
-  let temperature = document.querySelector(".temperature");
-  let description = document.querySelector(".description");
-  let iconElement = document.querySelector(".weather-icon");
-  let celsiusTemp = response.data.temperature.current;
-  iconElement.innerHTML = response.data.condition.icon;
-  temperature.innerHTML = Math.round(celsiusTemp);
-  description.innerHTML = response.data.condition.description;
-}
 
 setInterval(updateTimes, 1);
 updateTimes();
